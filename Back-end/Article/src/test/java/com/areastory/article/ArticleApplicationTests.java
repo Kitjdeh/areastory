d0bdec2a1f14a2e11c31320690da7790168f5a7d
sharksumin <1379psm@naver.com>
2023-04-17 13:25:46 +0900

    [BE] article 생성

---
+++ Back-end/Article/src/test/java/com/areastory/article/ArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.areastory.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
