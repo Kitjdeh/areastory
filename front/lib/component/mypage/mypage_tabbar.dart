1dad9cc31f4802a5e49909bff4667f9e6a064957
sky_rest <sky_rest@naver.com>
2023-04-25 00:37:00 +0900

    feat: add tabbar on mypage

---
+++ front/lib/component/mypage/mypage_tabbar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:front/constant/mypage_tabs.dart';
+
+class MypageTabbar extends StatefulWidget {
+  const MypageTabbar({Key? key}) : super(key: key);
+
+  @override
+  State<MypageTabbar> createState() => _MypageTabbarState();
+}
+
+class _MypageTabbarState extends State<MypageTabbar>
+    with TickerProviderStateMixin {
+  late final TabController mypagecontroller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    mypagecontroller = TabController(length: MYPAGETABS.length, vsync: this);
+
+    // 이건 나중에 데이터 작업할때
+    mypagecontroller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.green,
+        child: Column(
+          children: [
+            TabBar(
+              controller: mypagecontroller,
+              tabs: MYPAGETABS
+            .map((e) => Tab(icon: Icon(e.icon),)).toList()),
+            Expanded(
+              child: TabBarView(
+                controller: mypagecontroller,
+                  children: [Text("감자"), Text("고구마")]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ front/lib/constant/mypage_tabs.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TabInfo {
+  // 라벨(string)이 따로 표시되는게 없다.
+  // 현재 임시로 자체 Icons에 있는걸 사용하지만, 추후 svg로 다운받아서 asset폴더에서 가져와야한다.
+  final IconData icon;
+  final String label;
+
+  // 아마 새로운 게시글 생성하는것 관련은 공부를 해야한다.
+  const TabInfo({required this.icon, required this.label});
+}
+
+const MYPAGETABS = [
+  TabInfo(icon: Icons.book_outlined, label: '앨범'),
+  TabInfo(icon: Icons.map_outlined, label: '지도'),
+];
