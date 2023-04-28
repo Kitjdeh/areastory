d215712e599852b3c0adf717ec74254f11f7868a
박수민 <1379psm@naver.com>
2023-04-28 18:00:28 +0900

    [BE] fix : 전체 게시글 불러올때 공개된 게시물만 불러오기로 수정

--- Back-end/Article/src/main/java/com/areastory/article/dto/common/ArticleDto.java
+++ Back-end/Article/src/main/java/com/areastory/article/dto/common/ArticleDto.java
@@ -11,14 +11,6 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class ArticleDto extends ArticleCommonDto {
-    //    private Long articleId;
-//    private Long userId;
-//    private String nickname;
-//    private String profile;
-//    private String content;
-//    private String image;
-//    private Long likeCount;
-//    private Long commentCount;
     private String doName;
     private String si;
     private String gun;
@@ -37,30 +29,4 @@ public class ArticleDto extends ArticleCommonDto {
         this.eup = eup;
         this.myeon = myeon;
     }
-
-    //
-//        private Boolean isLike;
-//    private Boolean isOpen;
-//    private LocalDateTime createdAt;
-//
-//    public ArticleDto(Long articleId, Long userId, String nickname, String profile, String content, String image, Long likeCount, Long commentCount, String doName, String si, String gun, String gu, String dong, String eup, String myeon, Boolean isLike, Boolean isOpen, LocalDateTime createdAt) {
-//        this.articleId = articleId;
-//        this.userId = userId;
-//        this.nickname = nickname;
-//        this.profile = profile;
-//        this.content = content;
-//        this.image = image;
-//        this.likeCount = likeCount;
-//        this.commentCount = commentCount;
-//        this.doName = doName;
-//        this.si = si;
-//        this.gun = gun;
-//        this.gu = gu;
-//        this.dong = dong;
-//        this.eup = eup;
-//        this.myeon = myeon;
-//        this.isLike = isLike;
-//        this.isOpen = isOpen;
-//        this.createdAt = createdAt;
-////    }
 }
--- Back-end/Article/src/main/java/com/areastory/article/dto/common/ArticleRespDto.java
+++ Back-end/Article/src/main/java/com/areastory/article/dto/common/ArticleRespDto.java
@@ -11,17 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class ArticleRespDto extends ArticleCommonDto {
-    //    private Long articleId;
-//    private Long userId;
-//    private String nickname;
-//    private String profile;
-//    private String content;
-//    private String image;
-//    private Long likeCount;
-//    private Long commentCount;
     private String location;
-//    private Boolean isLike;
-//    private LocalDateTime createdAt;
 
     @Builder
     public ArticleRespDto(Long articleId, Long userId, String nickname, String profile, String content, String image, Long likeCount, Long commentCount, Boolean isLike, LocalDateTime createdAt, String location) {
