c0188fdb8c13321ed4f8b1b10461c3a918305854
sharksumin <1379psm@naver.com>
2023-04-30 15:21:58 +0900

    [BE] fix : 날짜 형식 변환 수정

--- Back-end/Article/src/main/java/com/areastory/article/dto/common/CommentDto.java
+++ Back-end/Article/src/main/java/com/areastory/article/dto/common/CommentDto.java
@@ -1,7 +1,9 @@
 package com.areastory.article.dto.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +17,8 @@ public class CommentDto {
     private String profile;
     private String content;
     private Long likeCount;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
     private Boolean likeYn;
 
