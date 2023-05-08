4d8cc3f455dd3ecd28f4066af76a4ea3bd84f25d
이상현 <dkrdj1000@naver.com>
2023-05-08 13:34:34 +0900

    [BE] fix : notification 설정

---
+++ Back-end/User/src/main/java/com/areastory/user/config/FCMConfig.java
@@ -0,0 +1,27 @@
+package com.areastory.user.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FCMConfig {
+    @PostConstruct
+    public void initFirebase() {
+        FileInputStream serviceAccount;
+        try {
+            serviceAccount = new FileInputStream("serviceAccountKey.json");
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
