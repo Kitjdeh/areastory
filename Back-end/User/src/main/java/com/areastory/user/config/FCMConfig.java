aabe434595cdb79a3ef341b4139ddb0f00b44f42
이상현 <dkrdj1000@naver.com>
2023-05-08 13:55:13 +0900

    [BE] fix : notification resource 에러 수정중

--- Back-end/User/src/main/java/com/areastory/user/config/FCMConfig.java
+++ Back-end/User/src/main/java/com/areastory/user/config/FCMConfig.java
@@ -3,13 +3,17 @@ package com.areastory.user.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.apache.commons.io.FileUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FCMConfig {
@@ -20,7 +24,14 @@ public class FCMConfig {
     public void initFirebase() {
         FileInputStream serviceAccount;
         try {
-            serviceAccount = new FileInputStream(resource.getFile());
+            InputStream inputStream = resource.getInputStream();
+            File serviceAccountKey = File.createTempFile("serviceAccountKey", ".json");
+            try {
+                FileUtils.copyInputStreamToFile(inputStream, serviceAccountKey);
+            } finally {
+                IOUtils.closeQuietly(inputStream);
+            }
+            serviceAccount = new FileInputStream(serviceAccountKey);
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
