8306d3b67c272f8545f27c9ccb19cb639e634516
박수민 <1379psm@naver.com>
2023-05-10 15:09:28 +0900

    [BE] fix : 웹소켓 수정, 카프카 수정

---
+++ Back-end/Article/src/main/java/com/areastory/article/dto/response/ChatMessageEnterResp.java
@@ -0,0 +1,21 @@
+package com.areastory.article.dto.response;
+
+import com.areastory.article.dto.common.ChatMessageDto;
+import com.areastory.article.dto.common.MessageType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
+public class ChatMessageEnterResp {
+    private MessageType type;
+    private Long userId;
+    private String roomId;
+    private String profile;
+    private String nickname;
+    private List<ChatMessageDto> messageList;
+    private Long userCount;
+
+}
