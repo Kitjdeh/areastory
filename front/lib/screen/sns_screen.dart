700766b52d3c7f0d522706c09772cec4e25a2422
sangjun <tkdwns9494@gmail.com>
2023-05-12 02:47:53 +0900

    feat:페이지네이션 적용

--- front/lib/screen/sns_screen.dart
+++ front/lib/screen/sns_screen.dart
@@ -37,43 +37,6 @@ Widget _myStory() {
   );
 }
 
-Widget _postList({
-  required int userId,
-  required void Function(int articleId) onDelete,
-  required int height,
-  required List articles,
-  required int currentPage,
-  required int lastPage,
-  required void Function() loadMoreData,
-}) {
-  return Column(
-    children: List.generate(
-      articles.length + 1,
-      (index) {
-        if (index < articles.length) {
-          print(index);
-          return ArticleComponent(
-            userId: userId,
-            onDelete: onDelete,
-            articleId: articles[index].articleId,
-            followingId: articles[index].userId,
-            height: 350,
-          );
-        } else if (currentPage < lastPage!) {
-          loadMoreData();
-          return Container(
-            height: 50,
-            alignment: Alignment.center,
-            child: const CircularProgressIndicator(),
-          );
-        }
-        return SizedBox
-            .shrink(); // Return an empty widget if the condition is not met
-      },
-    ),
-  );
-}
-
 Widget _storyBoardList() {
   return SingleChildScrollView(
     scrollDirection: Axis.horizontal,
@@ -101,7 +64,9 @@ Widget _storyBoardList() {
 
 class _SnsScreenState extends State<SnsScreen> {
   int _currentPage = 1;
-  int _lastPage = 0;
+  bool _hasNextPage = false;
+  bool _isFirstLoadRunning = false;
+  bool _isLoadMoreRunning = false;
   List _articles = [];
   String dropdownValue = list.first;
   String seletedLocationDosi = '';
@@ -109,14 +74,27 @@ class _SnsScreenState extends State<SnsScreen> {
   String seletedLocationDongeupmyeon = '';
 
   late final userId = int.parse(widget.userId);
+  late ScrollController _controller;
 
   @override
   void initState() {
     super.initState();
+    _controller = ScrollController();
     printArticles();
+    _controller.addListener(_loadMoreData);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_loadMoreData);
+    super.dispose();
   }
 
   void printArticles() async {
+    print('print');
+    setState(() {
+      _isFirstLoadRunning = true;
+    });
     if (widget.location != null) {
       List<String> locationParts = widget.location!.split(' ');
       if (locationParts.length == 1) {
@@ -149,25 +127,38 @@ class _SnsScreenState extends State<SnsScreen> {
       dongeupmyeon: seletedLocationDongeupmyeon,
     );
     _articles.addAll(articleData.articles);
-    _lastPage = articleData.totalPageNumber;
-    setState(() {});
+    _hasNextPage = articleData.nextPage;
+
+    setState(() {
+      _isFirstLoadRunning = false;
+    });
   }
 
   void _loadMoreData() async {
-    _currentPage++;
-    final newArticles = await getArticles(
-      sort: dropdownValue == '인기순' ? 'likeCount' : 'articleId',
-      page: _currentPage,
-      dosi: seletedLocationDosi,
-      sigungu: seletedLocationSigungu,
-      dongeupmyeon: seletedLocationDongeupmyeon,
-    );
-    _articles.addAll(newArticles.articles);
-    _lastPage = newArticles.totalPageNumber;
+    if (_hasNextPage == true &&
+        _isFirstLoadRunning == false &&
+        _isLoadMoreRunning == false &&
+        _controller.position.extentAfter < 3000) {
+      print('more');
+      setState(() {
+        _isLoadMoreRunning = true;
+      });
+      _currentPage += 1;
+      final newArticles = await getArticles(
+        sort: dropdownValue == '인기순' ? 'likeCount' : 'articleId',
+        page: _currentPage,
+        dosi: seletedLocationDosi,
+        sigungu: seletedLocationSigungu,
+        dongeupmyeon: seletedLocationDongeupmyeon,
+      );
+      _articles.addAll(newArticles.articles);
+      _hasNextPage = newArticles.nextPage;
 
-    setState(() {
-      // scrollToIndex(5);
-    });
+      setState(() {
+        print('성공');
+        _isLoadMoreRunning = false;
+      });
+    }
   }
 
   void onDelete(int articleId) {
@@ -198,6 +189,7 @@ class _SnsScreenState extends State<SnsScreen> {
   }
 
   void onChangeSort(String dropdownValue) {
+    print(_controller.position.extentAfter);
     setState(() {
       printArticles();
     });
@@ -260,24 +252,44 @@ class _SnsScreenState extends State<SnsScreen> {
           )
         ],
       ),
-      body: ListView(
-        children: [
-          widget.location != null
-              ? SizedBox(
-                  height: 0,
-                )
-              : _storyBoardList(),
-          _postList(
-            userId: userId,
-            onDelete: onDelete,
-            height: 350,
-            articles: _articles,
-            loadMoreData: _loadMoreData,
-            currentPage: _currentPage,
-            lastPage: _lastPage,
-          ),
-        ],
-      ),
+      body: _isFirstLoadRunning
+          ? const Center(
+              child: const CircularProgressIndicator(),
+            )
+          : ListView(
+              controller: _controller,
+              children: [
+                if (widget.location == null) _storyBoardList(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: List.generate(
+                    _articles.length,
+                    (index) => ArticleComponent(
+                      userId: userId,
+                      onDelete: onDelete,
+                      articleId: _articles[index].articleId,
+                      followingId: _articles[index].userId,
+                      height: 300,
+                    ),
+                  ),
+                ),
+                if (_isLoadMoreRunning)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, bottom: 40),
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  ),
+                if (!_isLoadMoreRunning && !_hasNextPage)
+                  Container(
+                    padding: const EdgeInsets.only(top: 30, bottom: 40),
+                    color: Colors.white,
+                    child: const Center(
+                      child: Text('더이상 게시글이 없습니다'),
+                    ),
+                  ),
+              ],
+            ),
     );
   }
 }
