3d4ece08d47c9033ba22fa2881ed4ed500646ae5
최현제 <qwas15788@gmail.com>
2023-05-11 17:21:32 +0900

    fix : notification Entity

--- Back-end/User/src/main/java/com/areastory/user/db/entity/Notification.java
+++ Back-end/User/src/main/java/com/areastory/user/db/entity/Notification.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,12 +15,14 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@DynamicInsert
 @Table(indexes = @Index(name = "idx_notification", columnList = "user_id, checked, notification_id"))
 public class Notification extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "notification_id")
     private Long notificationId;
+    @ColumnDefault("false")
     private Boolean checked;
     private String title;
     private String body;
@@ -27,6 +31,7 @@ public class Notification extends BaseTime {
     private Long commentId;
     @Column(name = "user_id")
     private Long userId;
+    private String type;
 
     public void check() {
         this.checked = true;
