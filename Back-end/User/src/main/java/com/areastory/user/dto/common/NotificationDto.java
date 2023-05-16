2fc989087427f1670a977d704c1789928fb369ee
박수민 <1379psm@naver.com>
2023-05-16 20:02:05 +0900

    [BE] fix: createdAt 시간 가공

--- Back-end/User/src/main/java/com/areastory/user/dto/common/NotificationDto.java
+++ Back-end/User/src/main/java/com/areastory/user/dto/common/NotificationDto.java
@@ -1,9 +1,11 @@
 package com.areastory.user.dto.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +18,8 @@ public class NotificationDto{
     private Boolean checked;
     private String title;
     private String body;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
     private Long articleId;
     private Long commentId;
